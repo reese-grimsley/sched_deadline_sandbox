ff50dea9af80882d26b247979303ce6ee9846a1b
Reese <rgrimsle@andrew.cmu.edu>
2021-09-25 14:09:51 -0400

    bugfixes

--- helpers.c
+++ helpers.c
@@ -19,10 +19,11 @@ struct timespec time_diff(const struct timespec * last_time, const struct timesp
      diff.tv_sec = current_time->tv_sec - last_time->tv_sec;
      diff.tv_nsec = current_time->tv_nsec - last_time->tv_nsec;
 
-     if (diff.tv_sec < 0)
+
+
+     if (diff.tv_nsec < 0 && diff.tv_sec == 0)
      {
-          diff.tv_sec = abs(diff.tv_sec);
-          diff.tv_nsec += 1000 * 1000 * 1000;
+          diff.tv_nsec = abs(diff.tv_nsec);
      }
 
      while (diff.tv_nsec < 0)
@@ -35,8 +36,6 @@ struct timespec time_diff(const struct timespec * last_time, const struct timesp
           diff.tv_nsec -= 1000 * 1000 * 1000;
           diff.tv_sec++;
      }
-
-
 
      return diff;
 }
